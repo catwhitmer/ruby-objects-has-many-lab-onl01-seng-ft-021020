ed262e4debc7803fdddf48da3149dcc86609ad37
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-25 13:53:13 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -21,10 +21,11 @@ class Author
   def add_post_by_title(title)
     post = Post.new(title)
     add_post(post)
+    @@post_count += 1
   end
   
   def self.post_count
-    
+    @@post_count
   end
   
 end
